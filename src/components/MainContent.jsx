242bc84298014d42793791a7b7bc528db5c5330f
Marsh-bp <bharathpranesh2004@gmail.com>
2025-01-06 22:50:15 +0530

    first commit

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react';
+import './MainContent.css';
+
+const MainContent = () => {
+  const [appointments, setAppointments] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchAppointments = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/appointments");
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log('Fetched Data:', data);
+        setAppointments(data); 
+        setLoading(false);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+
+    fetchAppointments();
+  }, []);
+
+  if (loading) return <p className='loading-text'>Loading...</p>;
+  if (error) return <p className='error-text'>Error: {error}</p>;
+
+  return (
+    <div className="main-content">
+      <h2>Appointment List</h2>
+      <table className="appointment-table">
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>UHID</th>
+            <th>Appointment Date</th>
+            <th>Time Slot</th>
+            <th>Department</th>
+            <th>Doctor</th>
+            <th>Appointment Type</th>
+            <th>Patient Type</th>
+          </tr>
+        </thead>
+        <tbody>
+          {appointments.map((appointment, index) => (
+            <tr key={index}>
+              <td>{appointment.patientName.split(" ")[0]}</td>
+              <td>{appointment.patientName.split(" ")[1]}</td>
+              <td>{appointment.UHID}</td>
+              <td>{appointment.appointmentDate}</td>
+              <td>{appointment.appointmentTime}</td>
+              <td>{appointment.department}</td>
+              <td>{appointment.doctorName}</td>
+              <td>General</td> {}
+              <td>Outpatient</td> {}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default MainContent;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Sidebar.css'; // Optional: For styling the sidebar
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="menu-icon">☰</div>
+    </div>
+  );
+};
+
+export default Sidebar;
